7263efaaa006c6be55af0d55af4998a45adb5c90
lavip120-a11y <lavip120@gmail.com>
2026-03-24 20:51:46 +1300

    update to server.js

--- database/server.js
+++ database/server.js
@@ -1,19 +1,22 @@
 const express = require("express"); //import express
+const cors = require("cors");
 const app = express(); //call express
 require("dotenv").config(); //to access .env information to run
-let dbConnect = require("./dbConnect");
+let dbConnect = require("./dbConnect"); //connects to the database
+//importing route files
 let userRoutes = require("./routes/userRoutes");
 let postRoutes = require("./routes/postRoutes");
 let commentRoutes = require("./routes/commentRoutes");
 let likeRoutes = require("./routes/likeRoutes");
 
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my Capstone Project Database application." }); //testing
 });
-
+//API routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
